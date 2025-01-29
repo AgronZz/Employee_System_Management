96759dae67a02362bc539778cdc8e3e24225f10e
aritaademi <aritaademi2003@gmail.com>
2025-01-29 13:25:03 +0100

    Added Users model, changed ApplicationDbContext

--- EmployeeManagementSystem/Data/ApplicationDbContext.cs
+++ EmployeeManagementSystem/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using EmployeeManagementSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeManagementSystem.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<Users>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { 
         
